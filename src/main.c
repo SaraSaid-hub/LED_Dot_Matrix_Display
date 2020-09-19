dc4620842defa74ba46b3ae42c54dc9caa27c93c
sara said <66921329+SaraSaid-hub@users.noreply.github.com>
2020-09-19 15:19:56 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,9 @@
+/*********************************************************/
+/* Author      : Sara Said                               */
+/* Date        : 17 SEPT 2020                            */
+/* Version     : V01                                     */
+/*********************************************************/
+
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
 
@@ -8,32 +14,23 @@
 
 #include "LEDMRX_interface.h"
 
-
-
-//u8 DataArray[8]={0,132,138,146,162,66,0,0};
 u8 DataArray[8]={0,38,73,73,73,50,0,0};
-u8 FullNameArray[32]={0, 38, 73, 73, 73, 50, 0, 0, 126, 9, 9, 126, 0, 0, 0, 0, 127, 17, 17, 17, 110, 0, 0, 0, 126, 9, 9, 126, 0, 0, 0, 0};
-u8 FullNameArray2[32]={0, 38, 73, 73, 73, 50, 0, 0, 126, 9, 9, 126, 0, 0, 127, 17, 17, 17, 110, 0, 0, 126, 9, 9, 126, 0, 0, 0, 0, 0, 0, 0};
 
 void main (void){
 
-
+        // Initialize System CLK
 	RCC_voidInitSysClock();
-	//PORTA
+	// Enable PORTA
 	RCC_voidEnableClock (RCC_APB2, 2);
-	//PORTB
+	//Enable PORTB
 	RCC_voidEnableClock (RCC_APB2, 3);
 
 	MSTK_voidInit();
 	HLEDMRX_voidInit();
-	// send data array to led matrix
-	//HLEDMRX_voidDisplay(DataArray);
-	HLEDMRX_voidFullDisplay(FullNameArray2);
-	// MGPIO_VoidSetPinValue (GPIOA, PIN0, 1);
+	// Send data array to led matrix
+	HLEDMRX_voidDisplay(DataArray);
 
 	while(1){
-
-
 
 	}
 }
